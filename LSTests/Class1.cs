260030ccb1fe21d38e2847cc0b34b05e3f18b10c
Maciej Warszawski <maciej@warszawski.pro>
2015-01-21 20:52:19 +0100

    draft obiektowo

--- LSTests/Class1.cs
+++ LSTests/Class1.cs
@@ -11,6 +11,64 @@ namespace LSTests
 
 
 
+
+    public class Counter
+    {
+        private int max = 0;
+        private int seq1 = 0;
+        private int seq2 = 0;
+        private bool switchAvailable = true;
+        private bool count1 = true;
+        private bool count2 = false;
+        private int p;
+        private int nonP;
+
+        public Counter(int p, int nonP)
+        {
+            this.p = p;
+            this.nonP = nonP;
+        }
+
+        public void Count(int i)
+        {
+
+            if (i == p)
+            {
+                seq1++;
+            }
+            if (i == p && count2)
+            {
+                seq2++;
+            }
+            if (i == nonP && !switchAvailable)
+            {
+                max = seq1;
+                seq2++;
+                seq1 = seq2;
+                seq2 = 0;
+            }
+            if (i == nonP && switchAvailable)
+            {
+                seq1++;
+                switchAvailable = false;
+                count2 = true;
+
+            }
+        }
+
+        public int Check()
+        {
+            int i = seq1;
+            if (switchAvailable)
+            {
+                i--;
+            }
+
+            return Math.Max(max, i);
+        }
+    }
+
+
     public class Ls
     {
 
@@ -18,43 +76,15 @@ namespace LSTests
         public static int Find(int[] sequence)
         {
 
-            int max = 0;
-            int seq1 = 0;
-            int seq2 = 0;
-            bool switchAvailable = true;
-            bool count1 = true;
-            bool count2 = false;
+            var oneCounter = new Counter(1, 0);
+            var zeroCounter = new Counter(0, 1);
 
             foreach (var i in sequence)
             {
-
-
-                if (i == 1)
-                {
-                    seq1++;
-                } 
-                if (i == 1 && count2)
-                {
-                    seq2++;
-                }
-                if (i == 0 && !switchAvailable)
-                {
-                    max = seq1;
-                    seq2++;
-                    seq1 = seq2;
-                    seq2 = 0;
-                }
-                if (i == 0 && switchAvailable)
-                {
-                    seq1++;
-                    switchAvailable = false;
-                    count2 = true;
-
-                }
-               
-
+                oneCounter.Count(i);
+                zeroCounter.Count(i);
             }
-            return Math.Max(max, seq1);
+            return Math.Max(oneCounter.Check(), zeroCounter.Check());
         }
     }
 
@@ -65,7 +95,6 @@ namespace LSTests
         public void MyTestMethod()
         {
             Assert.Equal(6, Ls.Find(new[] { 1, 0, 1, 1, 0, 1, 1, 1 }));
-
             Assert.Equal(8, Ls.Find(new[] { 1, 0, 1, 1, 1, 1, 1, 1 }));
             Assert.Equal(4, Ls.Find(new[] { 1, 0, 0, 0, 0, 1, 1, 1 }));
             //Assert.Equal(8, Ls.Find(new[] { 1, 1, 1, 1, 1, 1, 1, 1 }));        
